cf5f8e5b9d34ea62ddcb92f1202f99ecca05dbcc
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@GMAIL.COM>
2024-02-16 17:22:35 -0800

    link hotfix 2

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -9,7 +9,7 @@
     </header>
     <nav>
         <Link title="Github" url="https://github.com/m1chaelwilliams" icon="tabler:brand-github" />
-        <Link title="Linked In" url="www.linkedin.com/in/michael-williams-131773221" icon="tabler:brand-linkedin"/>
+        <Link title="Linked In" url="https://linkedin.com/in/michael-williams-131773221" icon="tabler:brand-linkedin"/>
         <Link title="YouTube" url="https://youtube.com/@codingwithsphere" icon="tabler:brand-youtube" />
         <Link title="Discord" url="https://discord.gg/3FxVWRDdXD" icon="tabler:brand-discord" />
         <Link title="Patreon" url="https://patreon.com/codingwithsphere" icon="tabler:brand-patreon" />
